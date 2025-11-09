a5be0e7498a38e1f64645a02279eb5e255fc7b0e
José Ryan <joseryan695@gmail.com>
2025-11-09 16:50:26 -0300

    .

---
+++ src/main/java/com/projetoVendas/sistema/controles/PrincipalController.java
@@ -0,0 +1,14 @@
+package com.projetoVendas.sistema.controles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class PrincipalController {
+
+    @GetMapping("/administrativo")
+    public String acessarPrincipal(){
+        return "administrativo/home";
+    }
+}
