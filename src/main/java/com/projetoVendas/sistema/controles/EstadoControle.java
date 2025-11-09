8e9a70ab52810942e1cf4f7c0038e37d9fefcffd
José Ryan <joseryan695@gmail.com>
2025-11-09 17:43:48 -0300

    .

---
+++ src/main/java/com/projetoVendas/sistema/controles/EstadoControle.java
@@ -0,0 +1,8 @@
+package com.projetoVendas.sistema.controles;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class EstadoControle {
+
+}
---
+++ src/main/java/com/projetoVendas/sistema/repositorios/EstadoRepositorio.java
@@ -0,0 +1,6 @@
+package com.projetoVendas.sistema.repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepositorio JpaRepository<Estado, Long> {
+}
