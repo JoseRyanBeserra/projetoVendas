c124e5fa7595ba74f81dba14979c46df4ece3a88
José Ryan <joseryan695@gmail.com>
2025-11-13 10:42:03 -0300

    .

--- src/main/java/com/projetoVendas/sistema/controles/EstadoControle.java
+++ src/main/java/com/projetoVendas/sistema/controles/EstadoControle.java
@@ -1,8 +1,24 @@
 package com.projetoVendas.sistema.controles;
 
+import com.projetoVendas.sistema.modelos.Estado;
+import com.projetoVendas.sistema.repositorios.EstadoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EstadoControle {
+
+    @Autowired
+    private EstadoRepositorio estadoRepositorio;
+
+    @GetMapping("/cadastroEstado")
+    public ModelAndView cadastrar(Estado estado){
+        ModelAndView mv = new ModelAndView("/administrativo/estado/cadastro");
+        mv.addObject("estado",estado);
+        return mv;
+    }
 
 }
--- src/main/java/com/projetoVendas/sistema/repositorios/EstadoRepositorio.java
+++ src/main/java/com/projetoVendas/sistema/repositorios/EstadoRepositorio.java
@@ -1,6 +1,7 @@
 package com.projetoVendas.sistema.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import com.projetoVendas.sistema.modelos.Estado;
 
-public interface EstadoRepositorio JpaRepository<Estado, Long> {
+public interface EstadoRepositorio extends JpaRepository<Estado, Long> {
 }
